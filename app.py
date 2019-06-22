9394ed3196517f4fa09d8a13749c9c3de472d09b
willy <nillyw31@gmail.com>
2019-06-22 12:47:51 +0300

    added a navigation bar

--- app.py
+++ app.py
@@ -1,12 +1,30 @@
-from flask import Flask
-
+from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
-
+posts = [
+    {
+        'author': 'Wilfred',
+        'title': 'Go home',
+        'content': 'I am good',
+        'date_posted': 'July 28 2019'
+    },
+    {
+        'author': 'Kid',
+        'title': 'Go home',
+        'content': 'I am good',
+        'date_posted': 'July 27 2019'
+    }
+]
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/')
+def home():
+    return render_template('home.html', posts=posts)
+
+
+@app.route('/about')
+def about():
+    return render_template('about.html',title='About page')
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
